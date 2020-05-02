68b4ba13ade31f67be146abe8f52f9cf5fc7cc36
unknown <dethgrenade@gmail.com>
2020-05-01 20:46:29 -0700

    Navbar bare bones in place

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ const Navbar = props => {
 
     return(
         <nav className="navbar">
+            <div className="menubar"></div>
+            <div className="menubar shadowbox"></div>
         </nav>
     )
 }
