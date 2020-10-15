33b19847ee51a8e9eeb2ce2af71ee08ce79459f1
unknown <dethgrenade@gmail.com>
2020-10-15 14:30:07 -0700

    added Loading Spinner to / landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 //Components
 import Navbar from "./Navbar";
 
+import LoadingSpinner from "./LoadingSpinner";
+
 import HeroMenu from "./hero-menus/HeroMenu";
 import HeroGuide from "./hero-guide/HeroGuide";
 import AwakeningGuide from "./awakenings/AwakeningGuide";
@@ -23,6 +25,7 @@ const App = () => {
   return (
     <Router>
       <Navbar />
+      <Route exact path="/" component={LoadingSpinner} />
       <Route exact path="/(heroes|awakenings)/" component={HeroMenu} />
       <Route exact path="/heroes/:hero" component={HeroGuide} />
       <Route exact path="/awakenings/:hero" component={AwakeningGuide} />
