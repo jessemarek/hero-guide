7c3412764481b48005ee45e285812ecc0a22344e
unknown <dethgrenade@gmail.com>
2020-10-16 17:36:02 -0700

    added attributes to ability icons

--- src/components/hero-guide/AbilityBox.jsx
+++ src/components/hero-guide/AbilityBox.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import { returnType } from "../../utils/index";
 
 const AbilityBox = ({ hero, ability, type }) => {
+  const attr = ability.attributes;
+
   return (
     <div className="ability-box">
       <h4>
@@ -12,10 +14,19 @@ const AbilityBox = ({ hero, ability, type }) => {
 
       <div className="ability-details">
         <div className="ability-icon">
-          <img
-            src={`/assets/images/icons/abilities/${hero}-ability${type}.png`}
-            alt="ability icon"
-          />
+          <span>
+            <img
+              src={`/assets/images/icons/abilities/${hero}-ability${type}.png`}
+              alt="ability icon"
+            />
+            {attr && attr !== "none" ? (
+              <img
+                className="ability-attribute"
+                src={`/assets/images/icons/attributes/${attr}.png`}
+                alt=""
+              />
+            ) : null}
+          </span>
         </div>
 
         <div className="ability-text">
